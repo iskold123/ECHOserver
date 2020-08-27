f246aeb303ef90e0cabcf5197ae600d589a72ef9
Mads Olsen <mads36g6@edu.easj.dk>
2020-08-27 11:26:49 +0200

    Client er oprettet.

---
+++ EchoServer/EchoClient/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient
+{
+    class Client
+    {
+        public void Start()
+        {
+            TcpClient socket = new TcpClient("localhost",7777);
+
+            StreamReader sr = new StreamReader(socket.GetStream());
+            StreamWriter sw = new StreamWriter(socket.GetStream());
+
+            String message = "Mads";
+            sw.WriteLine(message);
+            sw.Flush();
+
+            String returnmessage = sr.ReadLine();
+            Console.WriteLine($"Retur fra server: {returnmessage}");
+
+            socket.Close();
+
+        }
+
+
+    }
+}
